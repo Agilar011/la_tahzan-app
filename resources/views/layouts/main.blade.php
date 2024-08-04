32756b70596e3ce145a263e35e74f87763a6d772
Agilar Gumilar <agilar.011@gmail.com>
2024-08-04 21:47:36 +0700

    fix footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -149,71 +149,60 @@
                 <p>&copy; 2021 All Rights Reserved</p>
             </footer> --}}
 
-    <footer class="bg-slate-800 px-4 py-2">
-        <div class="flex justify-between items-center text-white">
-            <!-- Logo Section -->
-            <div>
-                <img src="/img/la tahzan.png" alt="Logo" class="h-12">
-            </div>
-            <ul>
-                <p class="font-bold text-xl">Produk Kami</p>
-                <li>Umrah</li>
-                <li>otomotif</li>
-                <li>Property</li>
-            </ul>
+            <footer class="footer py-2 bg-slate-800">
+                <div class="container mx-auto px-4">
+                    <div class="flex flex-wrap ">
+                        <div class="flex w-full sm:w-1/2 md:w-1/5 px-4  text-white justify-center place-items-center">
+                            <img src="/img/la tahzan.png" alt="Logo" class="h-[10vh] w-auto mx-auto my-auto object-contain">
+                        </div>
+                        <div class="w-full sm:w-1/2 md:w-1/5 px-4  text-white    ">
+                            <h4
+                                class="text-lg font-medium mb-6 relative pb-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-12 after:h-0.5 after:bg-white">
+                                company
+                            </h4>
 
-            <ul>
-                <p class="font-bold text-xl">Tentang Kami</p>
-                <li>About</li>
-                <li>Kebijakan</li>
-                <li>Layanan Periklanan</li>
-            </ul>
+                        </div>
+                        <div class="w-full sm:w-1/2 md:w-1/5 px-4  text-white">
+                            <h4
+                                class="text-lg font-medium mb-6 relative pb-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-12 after:h-0.5 after:bg-white">
+                                get help
+                            </h4>
 
-            <ul>
-                <p class="font-bold text-xl">Hubungi Kami</p>
-                <li>Alamat</li>
-                <li>Email</li>
-                <li>Telepon</li>
+                        </div>
+                        <div class="w-full sm:w-1/2 md:w-1/5 px-4  text-white">
+                            <h4
+                                class="text-lg font-medium mb-6 relative pb-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-12 after:h-0.5 after:bg-white">
+                                online shop
+                            </h4>
 
-                <!-- Hamburger Menu (Mobile) -->
-                {{-- <div class="md:hidden">
-                        <button id="menu-toggle" class="text-white focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-                            </svg>
-                        </button>
+                        </div>
+                        <div class="w-full sm:w-1/2 md:w-1/5 px-4  text-white">
+                            <h4
+                                class="text-lg font-medium mb-6 relative pb-2 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-12 after:h-0.5 after:bg-white">
+                                follow us
+                            </h4>
+                            <div class="flex space-x-4">
+                                <a href="#"
+                                    class="w-10 h-10 flex items-center justify-center bg-white bg-opacity-20 text-white rounded-full hover:bg-white hover:text-gray-900 transition duration-300">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                                <a href="#"
+                                    class="w-10 h-10 flex items-center justify-center bg-white bg-opacity-20 text-white rounded-full hover:bg-white hover:text-gray-900 transition duration-300">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                                <a href="#"
+                                    class="w-10 h-10 flex items-center justify-center bg-white bg-opacity-20 text-white rounded-full hover:bg-white hover:text-gray-900 transition duration-300">
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                                <a href="#"
+                                    class="w-10 h-10 flex items-center justify-center bg-white bg-opacity-20 text-white rounded-full hover:bg-white hover:text-gray-900 transition duration-300">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-
-                    <!-- footerigation Links (Desktop) -->
-                    <div class="hidden md:block px-2 py-2">
-                        <ul class="flex space-x-10">
-                            <li><a href="/about" class="text-white">Umrah</a></li>
-                            <li><a href="/services" class="text-white">Property</a></li>
-                            <li><a href="/contact" class="text-white">Otomotif</a></li>
-                        </ul>
-                    </div>
-
-                    <!-- Authentication Links (Desktop) -->
-                    <div class="hidden md:block">
-                        <ul class="flex space-x-4">
-                            <li><a href="/login" class="text-white">Login</a></li>
-                            <li><a href="/register" class="text-white">Register</a></li>
-                        </ul>
-                    </div> --}}
-        </div>
-
-        <!-- Navigation Links (Mobile) -->
-        {{-- <div id="mobile-menu" class="hidden md:hidden">
-                    <ul class="flex flex-col space-y-4 mt-4">
-                        <li><a href="/" class="text-white">Home</a></li>
-                        <li><a href="/about" class="text-white">Umrah</a></li>
-                        <li><a href="/services" class="text-white">Property</a></li>
-                        <li><a href="/contact" class="text-white">Otomotif</a></li>
-                        <li><a href="/login" class="text-white">Login</a></li>
-                        <li><a href="/register" class="text-white">Register</a></li>
-                    </ul>
-                </div> --}}
-    </footer>
+                </div>
+            </footer>
 
     @stack('modals')
 
